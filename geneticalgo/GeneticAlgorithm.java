a84180edf3318e758726300187e80518727f3dcd
Peter Camilleri <pdc.cse@gmail.com>
2012-05-20 14:26:03 +1000

    Initial commit

---
+++ geneticalgo/GeneticAlgorithm.java
@@ -0,0 +1,11 @@
+package geneticalgo;
+
+public class GeneticAlgorithm {
+	
+	public void execute() {
+		System.out.println("Doing magic...");
+		// do magic
+		System.out.println("Magic complete!");
+	}
+
+}
---
+++ geneticalgo/Main.java
@@ -0,0 +1,21 @@
+package geneticalgo;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Starting program...");
+
+		// 1) read input data from file into some data structure
+		
+		// 2) parse the input data into a usable format
+		// (perhaps combine with step 1 at later stage)
+		
+		// 3) Do Genetic Algo Magic
+		
+		GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
+		geneticAlgorithm.execute();
+		
+		System.out.println("Closing program...");
+	}
+
+}
