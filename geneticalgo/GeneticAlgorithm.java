db1d373837cc9a5e39cc150cd523bc7345257193
Peter Camilleri <pdc.cse@gmail.com>
2012-05-20 23:16:55 +1000

    added some more skeleton code + slight refactoring

--- geneticalgo/GeneticAlgorithm.java
+++ geneticalgo/GeneticAlgorithm.java
@@ -20,41 +20,51 @@ import java.util.Map;
 
 public class GeneticAlgorithm {
 
+
     /**
-     * GA(Fitness, Fitness_Threshold, p, r, m)
+     * 
+     * @param fitnessThreshold - point at which the algorithm will stop execution
+     * @param p - number of initial hypothesis to generate
+     * @param r - some value ???
+     * @param m - some value ???
      */
-    public void execute(int fitnessThreshold) {
+    public void execute(int fitnessThreshold, int p, int r, int m) {
 
         // initialise: P <- p random hypothesis
         // hypothesis: maps bitStrings to fitness
-        Map<String, Integer> hypothesis = new HashMap<String, Integer>();
-        generateRandomBitStrings(hypothesis);
+        Map<String, Integer> hypothesises = new HashMap<String, Integer>();
+        generateRandomBitStrings(hypothesises);
 
         // evaluate: for each h in P, compute fitness(h)
-        for (String s : hypothesis.keySet()) {
-            hypothesis.put(s, FitnessFunction.fitness(s));
+        for (String h : hypothesises.keySet()) {
+            hypothesises.put(h, FitnessFunction.fitness(h));
         }
         // TODO: combine above steps for efficiency. i.e. get the fitness value
         // as bitStrings are put into the map
 
-        while (SetUtilClass.maxFitness(hypothesis) < fitnessThreshold) {
-            // while [max,,h,, fitness(h)] < fitness_threshold
-            // do
+        // while [max,,h,, fitness(h)] < fitness_threshold
+        while (SetUtilClass.maxFitness(hypothesises) < fitnessThreshold) {
+            
+            // the set Ps in the comments
+            Map<String, Integer> nextHypothesises = new HashMap<String, Integer>();
+            
             // select: probabilistically select (1-r)p members of P to add to Ps
             // Prob(h,,i,,) = Fitness(h,,i,,) / sum from j=1 to p of (Fitness
             // h,,j,,)
             // so the probability that hypothesis h,,i,, will be selected is
             // the fitness of h,,i,, divided by the sum of all fitnesses of al
             // hypothesis???
-            // TODO: need to check the above formula
-            //
+            // TODO: need to check the above formula, what does it mean?
+            selectNewGeneration();
 
             // crossover: probabilistically select r-p/2 pairs of hypos from P.
             // produce two offspring for each pair using crossover operator, add
             // to Ps
+            crossover(hypothesises, nextHypothesises);
 
             // mutate: invert a randomly selected bit in m * p random hypos from
             // set Ps
+            mutate();
 
             // update: P <- Ps
 
@@ -65,17 +75,53 @@ public class GeneticAlgorithm {
         // at this point, atleast one one hypothesis in P is above the threshold
 
         // return the hypo with highest fitness in P
-        String best = SetUtilClass.maxHypothesis(hypothesis);
+        String best = SetUtilClass.maxHypothesis(hypothesises);
 
-        System.out.println("Best hypo:" + best);
+        System.out.println("Best hypo: " + best);
 
+    }
+
+    /**
+     *    // mutate: invert a randomly selected bit in m * p random hypos from
+            // set Ps
+     */
+    private void mutate() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+     * implementation of all of this.
+     * basically selecting new hypothesises for the next generation of the algorithm
+     * 
+            // select: probabilistically select (1-r)p members of P to add to Ps
+            // Prob(h,,i,,) = Fitness(h,,i,,) / sum from j=1 to p of (Fitness
+            // h,,j,,)
+            // so the probability that hypothesis h,,i,, will be selected is
+            // the fitness of h,,i,, divided by the sum of all fitnesses of al
+            // hypothesis???
+            // TODO: need to check the above formula, what does it mean?
+     */
+    private void selectNewGeneration() {
+        // TODO: implement 
     }
 
     /**
      * TODO should this go here? or in some sort of util class?
      */
     private void generateRandomBitStrings(Map<String, Integer> hypothesis) {
-        hypothesis.put("1001110001", null);
+        hypothesis.put("000000000000000000000000000", null);
     }
 
+    /**
+     *             // crossover: probabilistically select r-p/2 pairs of hypos from P.
+            // produce two offspring for each pair using crossover operator, add
+            // to Ps
+     * @param hypothesises
+     * @param nextHypothesises
+     */
+    private void crossover(Map<String, Integer> hypothesises, Map<String, Integer> nextHypothesises) {
+        // TODO Auto-generated method stub
+        
+    }
 }
--- geneticalgo/Main.java
+++ geneticalgo/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		// 3) Do Genetic Algo Magic
 		
 		GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
-		geneticAlgorithm.execute();
+		geneticAlgorithm.execute(0, 0, 0, 0);
 		
 		System.out.println("Closing program...");
 	}
