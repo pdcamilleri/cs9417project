24a2dcb8e4f90b305be8a0e2559322b0f09520fc
Peter Camilleri <pdc.cse@gmail.com>
2012-05-21 22:40:58 +1000

    Basic implementation of crossover command, it has some issues, see the TODO comments
    Also note, program seems to converge to values within the 3-6 range. So good change of infinite loops.

--- geneticalgo/GeneticAlgorithm.java
+++ geneticalgo/GeneticAlgorithm.java
@@ -45,21 +45,21 @@ public class GeneticAlgorithm {
         while (SetUtilClass.maxFitness(hypothesises) < fitnessThreshold) {
             
             // the set Ps in the comments
-            Map<String, Integer> nextHypothesises = getRandomBitStrings(p);
+            Map<String, Integer> nextHypothesises = new HashMap<String, Integer>(p);
 
             // select: probabilistically select (1-r)p members of P to add to Ps
             // Prob(h,,i,,) = Fitness(h,,i,,) / sum from j=1 to p of (Fitness
             // h,,j,,)
             // so the probability that hypothesis h,,i,, will be selected is
-            // the fitness of h,,i,, divided by the sum of all fitnesses of al
+            // the fitness of h,,i,, divided by the sum of all fitnesses of all
             // hypothesis???
             // TODO: need to check the above formula, what does it mean?
-            selectNewGeneration();
+            selectNewGeneration(hypothesises, nextHypothesises); // weed out the weak, keep the strong
 
             // crossover: probabilistically select r-p/2 pairs of hypos from P.
             // produce two offspring for each pair using crossover operator, add
             // to Ps
-            crossover(hypothesises, nextHypothesises);
+            crossover(hypothesises, nextHypothesises, p, r);
 
             // mutate: invert a randomly selected bit in m * p random hypos from
             // set Ps
@@ -67,12 +67,12 @@ public class GeneticAlgorithm {
 
             // update: P <- Ps
             hypothesises = nextHypothesises;
-            // calculate the fitnesses again (TODO: functionize this more since we use it twice)
+            
+            // evaluate for each h in P, compute fitness(h)
+            // TODO: functionize this more since we use it twice
             for (String h : hypothesises.keySet()) {
                 hypothesises.put(h, FitnessFunction.fitness(h));
             }
-
-            // evaluate for each h in P, compute fitness(h)
 
             // done - end of while loop
         }
@@ -105,9 +105,11 @@ public class GeneticAlgorithm {
             // the fitness of h,,i,, divided by the sum of all fitnesses of al
             // hypothesis???
             // TODO: need to check the above formula, what does it mean?
+     * @param nextHypothesises 
+     * @param hypothesises 
      */
-    private void selectNewGeneration() {
-        // TODO: implement 
+    private void selectNewGeneration(Map<String, Integer> hypothesises, Map<String, Integer> nextHypothesises) {
+        nextHypothesises.putAll(hypothesises);
     }
 
     /**
@@ -133,14 +135,53 @@ public class GeneticAlgorithm {
     }
 
     /**
-     *             // crossover: probabilistically select r-p/2 pairs of hypos from P.
-            // produce two offspring for each pair using crossover operator, add
-            // to Ps
+     * crossover: probabilistically select r-p/2 pairs of hypos from P.
+     * produce two offspring for each pair using crossover operator, add
+     * to Ps
      * @param hypothesises
      * @param nextHypothesises
+     * @param r 
+     * @param p 
      */
-    private void crossover(Map<String, Integer> hypothesises, Map<String, Integer> nextHypothesises) {
-        // TODO Auto-generated method stub
-        
+    private void crossover(Map<String, Integer> hypothesises, Map<String, Integer> nextHypothesises, int p, int r) {
+        int numPairs = (r-p) / 2; // TODO: how to do this "probabilistically"?
+        Random random = new Random();
+        String[] genePool = new String[10];
+        hypothesises.keySet().toArray(genePool);
+        // perform crossovers
+        for (int i = 0; i < numPairs; i++) {
+            // select two random parents (note, mother == father is possible)
+            // TODO: check whether or not the above is a problem. probably will be, since overpopulating pool with the same gene.
+            String mother = genePool[random.nextInt(genePool.length)]; 
+            String father = genePool[random.nextInt(genePool.length)];
+            int crossoverPoint = random.nextInt(mother.length());
+            
+            // let the mating begin!
+            // TODO: check this. as it stands, last bit of mother will always be accepted. guess it makes sense, feels wrong tho
+            String boy = father.substring(0, crossoverPoint - 1) + mother.substring(crossoverPoint);
+            String girl = mother.substring(0, crossoverPoint - 1) + father.substring(crossoverPoint);
+            nextHypothesises.put(boy, null);
+            nextHypothesises.put(girl, null);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
