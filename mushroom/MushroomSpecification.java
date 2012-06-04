e5ceb66cfd05c879cae878bdb5f4b26678f9c687
lachd <lachlan.dally@gmail.com>
2012-06-04 14:53:03 +1000

    altered generateHypothesis to not pull in length of hypothesis as that is core to the specification

--- mushroom/MushroomSpecification.java
+++ mushroom/MushroomSpecification.java
@@ -1,21 +1,27 @@
 package mushroom;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 import geneticalgo.FitnessFunction;
 import geneticalgo.ProblemSpecification;
 
 public class MushroomSpecification implements ProblemSpecification {
 
+    private static final int LENGTH_OF_HYPOTHESIS = 127;
+	
     public FitnessFunction getFitnessFunction() {
         return new MushoomFitnessFunction();
     }
-    
-    public Map<String, Integer> generateHypotheses(int p, int lengthOfBitStrings) {
-    	Map<String, Integer> map = new HashMap<String, Integer>();
+
+	public Map<String, Integer> generateHypotheses(int p) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
         for (int i = 0; i < p; i++) {
-    //            String bitString = getRandomBitString();
+//            String bitString = getRandomBitString();
             Random r = new Random();
             String s = "";
-            for (int j = 0; j < lengthOfBitStrings; j++) { // TODO what is the length of each hypo? needs to be a variable
+            for (int j = 0; j < LENGTH_OF_HYPOTHESIS; j++) { // TODO what is the length of each hypo? needs to be a variable
                 if (r.nextBoolean()) {
                     s += "1";
                 } else {
@@ -25,7 +31,6 @@ public class MushroomSpecification implements ProblemSpecification {
             map.put(s, null);
         }
         return map;
-    }
-
+	}
 
 }
