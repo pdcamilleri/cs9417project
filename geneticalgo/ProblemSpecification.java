4f2ba9578a41763bec77775abeea6b12cffc408c
lachd <lachlan.dally@gmail.com>
2012-06-04 13:25:50 +1000

    added problem specification to master

---
+++ geneticalgo/ProblemSpecification.java
@@ -0,0 +1,17 @@
+package geneticalgo;
+
+import java.util.Map;
+
+/**
+ * Interface which algorithms must implement in order to use the GeneticAlgorithm class.
+ * This will include things like how the bitstring represents the hypothesis, how the fitness function works, etc.
+ * basically it will include all key parts of the algorithm that are non generic.
+ *
+ */
+public interface ProblemSpecification {
+    
+    FitnessFunction getFitnessFunction();
+    
+    Map<String, Integer> generateHypotheses (int p, int lengthOfBitStrings);
+    
+}
