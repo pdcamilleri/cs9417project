4e8b947c0e5cf07c14d4006ca8e2a6dfb9a7005f
Peter Camilleri <pdc.cse@gmail.com>
2012-06-04 22:42:37 +1000

    Changed main so that each problem can have its own set of parameters

--- geneticalgo/Main.java
+++ geneticalgo/Main.java
@@ -4,28 +4,40 @@ import balanceScale.BalanceScaleSpecification;
 import mushroom.MushroomSpecification;
 
 public class Main {
-	
-	public static void main(String[] args) {
 
-		ProblemSpecification problemSpecification = null;
-	
-		if (args.length == 0) {
-		    System.out.println("Must have command line argument of \"balanceScale\" or \"mushroom\"");
-		    return;
-		}
-		
-		if (args[0].startsWith("b")) {
-		    problemSpecification = new BalanceScaleSpecification();
-		} else if (args[0].startsWith("m")) {
-			problemSpecification = new MushroomSpecification();
-		}
-		
-		GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm(problemSpecification);
-		//                       fT  pop   xo   mut
-		//                       fT    p    r     m
-		geneticAlgorithm.execute(9000, 100, 0.2, 0.05);
-		
-		System.out.println("Closing program...");
-	}
+    public static void main(String[] args) {
+
+        ProblemSpecification problemSpecification = null;
+
+        if (args.length == 0) {
+            System.out
+                    .println("Must have command line argument of \"balanceScale\" or \"mushroom\"");
+            return;
+        }
+
+        int threshold = 0;
+        int populationSize = 0;
+        double crossoverRate = 0;
+        double mutationRate = 0;
+
+        if (args[0].startsWith("b")) {
+            problemSpecification = new BalanceScaleSpecification();
+            threshold = 600;
+            populationSize = 50;
+            crossoverRate = 0.1;
+            mutationRate = 0.01;
+        } else if (args[0].startsWith("m")) {
+            problemSpecification = new MushroomSpecification();
+            threshold = 55;
+            populationSize = 50;
+            crossoverRate = 0.2;
+            mutationRate = 0.05;
+        }
+
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm(problemSpecification);
+        geneticAlgorithm.execute(threshold, populationSize, crossoverRate, mutationRate);
+
+        System.out.println("Closing program...");
+    }
 
 }
