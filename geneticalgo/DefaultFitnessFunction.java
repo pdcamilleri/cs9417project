c40398b0352681ef09de47b69bb917c775ec5ee8
lachd <lachlan.dally@gmail.com>
2012-06-04 13:26:27 +1000

    added default fitness function for maxing 1s problem spec

---
+++ geneticalgo/DefaultFitnessFunction.java
@@ -0,0 +1,19 @@
+package geneticalgo;
+
+public class defaultFitnessFunction implements FitnessFunction {
+
+  public int getFitness(String hypothesis) {
+		  int count = 0;
+	      for (int i = 0; i < hypothesis.length(); i++) {
+	              count += (hypothesis.charAt(i) - '0');
+	      }
+	      return count;
+	}
+
+	public StringBuffer hypothesisToGrepString(String hypothesis) {
+		StringBuffer toPrint = new StringBuffer("");
+		toPrint.append(hypothesis);
+		return toPrint;
+	}
+
+}
