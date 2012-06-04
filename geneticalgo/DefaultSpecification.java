172efdd2b89fbe0c5882e12c1c3ae0f51939668a
lachd <lachlan.dally@gmail.com>
2012-06-04 14:55:17 +1000

    removed length of hypothesis from genHypos function

--- geneticalgo/DefaultSpecification.java
+++ geneticalgo/DefaultSpecification.java
@@ -8,17 +8,19 @@ import java.util.Random;
 
 public class defaultSpecification implements ProblemSpecification {
 
-  public FitnessFunction getFitnessFunction() {
+	private static final int LENGTH_OF_HYPOTHESIS = 100;
+	
+	public FitnessFunction getFitnessFunction() {
 		return new defaultFitnessFunction();
 	}
 
-	public Map<String, Integer> generateHypotheses (int p, int lengthOfBitStrings) {
+	public Map<String, Integer> generateHypotheses (int p) {
 		 Map<String, Integer> map = new HashMap<String, Integer>();
 	        for (int i = 0; i < p; i++) {
 //	            String bitString = getRandomBitString();
 	            Random r = new Random();
 	            String s = "";
-	            for (int j = 0; j < lengthOfBitStrings; j++) { // TODO what is the length of each hypo? needs to be a variable
+	            for (int j = 0; j < LENGTH_OF_HYPOTHESIS; j++) { // TODO what is the length of each hypo? needs to be a variable
 	                if (r.nextBoolean()) {
 	                    s += "1";
 	                } else {
